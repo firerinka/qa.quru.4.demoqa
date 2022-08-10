5f6f1f8b15a0638a78dff2421af3566f39139bc1
Marina Remneva <marinaremneva@gmail.com>
2022-08-10 19:30:44 +0300

    fixes after homework discussion

--- src/test/java/com/demoqa/PracticeFormTests.java
+++ src/test/java/com/demoqa/PracticeFormTests.java
@@ -5,8 +5,7 @@ import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -20,10 +19,10 @@ public class PracticeFormTests {
     static final String PHONE = "9992223344";
     static final String BIRTH_YEAR = "2000";
     static final String BIRTH_MONTH = "September";
-    static final String BIRTH_DAY = "15";
+    static final String BIRTH_DAY = "30";
     static final String SUBJECT = "Maths";
     static final String HOBBY = "Reading";
-    static final String PICTURE_PATH = "src/test/resources/testPicture.jpg";
+    static final String PICTURE_PATH = "img/testPicture.jpg";
     static final String CURRENT_ADDRESS = "Private road 1";
     static final String STATE = "Haryana";
     static final String CITY = "Karnal";
@@ -31,12 +30,16 @@ public class PracticeFormTests {
     @BeforeAll
     static void configure() {
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "760x840";
+        Configuration.browserSize = "1920x1080";
     }
 
     @Test
     void studentRegistrationFormTest() {
         open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
 
         $("#firstName").setValue(FIRST_NAME);
         $("#lastName").setValue(LAST_NAME);
@@ -45,13 +48,13 @@ public class PracticeFormTests {
         $("#userNumber").setValue(PHONE);
 
         $("#dateOfBirthInput").click();
-        $("[class=\"react-datepicker__year-select\"]").selectOption(BIRTH_YEAR);
-        $("[class=\"react-datepicker__month-select\"]").selectOption(BIRTH_MONTH);
-        $("[class~=\"react-datepicker__day--0" + BIRTH_DAY + "\"]").click();
+        $(".react-datepicker__year-select").selectOption(BIRTH_YEAR);
+        $(".react-datepicker__month-select").selectOption(BIRTH_MONTH);
+        $(".react-datepicker__day--0" + BIRTH_DAY + ":not(.react-datepicker__day--outside-month)").click();
 
         $("#subjectsInput").setValue(SUBJECT).pressEnter();
         $("#hobbiesWrapper").$(byText(HOBBY)).click();
-        $("#uploadPicture").uploadFile(new File(PICTURE_PATH));
+        $("#uploadPicture").uploadFromClasspath(PICTURE_PATH);
         $("#currentAddress").setValue(CURRENT_ADDRESS);
 
         SelenideElement stateElement = $("#stateCity-wrapper").$("#state");
@@ -62,7 +65,10 @@ public class PracticeFormTests {
         cityElement.click();
         cityElement.$(byText(CITY)).click();
 
-        $("#submit").pressEnter();
+        $("#submit").click();
+
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
 
         checkResult("Student Name", FIRST_NAME + " " + LAST_NAME);
         checkResult("Student Email", EMAIL);
@@ -76,8 +82,8 @@ public class PracticeFormTests {
         checkResult("State and City", STATE + " " + CITY);
     }
 
-    public void checkResult(String key, String value) {
-        $(".table-responsive").$(byText(key))
-                .parent().shouldHave(text(value));
+    public void checkResult(String label, String values) {
+        $(".table-responsive").$(byText(label))
+                .parent().shouldHave(text(values));
     }
 }
