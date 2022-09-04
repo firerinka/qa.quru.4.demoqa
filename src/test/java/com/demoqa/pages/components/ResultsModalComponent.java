4c287a718f12b35fe438781e3aaf888a20dc3b93
Marina Remneva <marinaremneva@gmail.com>
2022-09-04 20:08:00 +0300

    adding steps for form POs

--- src/test/java/com/demoqa/pages/components/ResultsModalComponent.java
+++ src/test/java/com/demoqa/pages/components/ResultsModalComponent.java
@@ -1,5 +1,7 @@
 package com.demoqa.pages.components;
 
+import io.qameta.allure.Step;
+
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
@@ -9,12 +11,14 @@ public class ResultsModalComponent {
 
     private static final String MODAL_TITLE = "Thanks for submitting the form";
 
+    @Step("Проверяем появление окна с результатами")
     public ResultsModalComponent checkModalFormAppeared() {
         $(".modal-dialog").should(appear);
         $("#example-modal-sizes-title-lg").shouldHave(text(MODAL_TITLE));
         return this;
     }
 
+    @Step("Проверяем, что Label '{label}' имеет Values '{values}'")
     public ResultsModalComponent checkResult(String label, String values) {
         $(".table-responsive").$(byText(label))
                 .parent().shouldHave(text(values));
