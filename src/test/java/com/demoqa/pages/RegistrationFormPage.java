c732d254b74431ca788d431f6f2a2173f9829248
Marina Remneva <marinaremneva@gmail.com>
2022-09-19 19:29:18 +0300

    adding elements

--- src/test/java/com/demoqa/pages/RegistrationFormPage.java
+++ src/test/java/com/demoqa/pages/RegistrationFormPage.java
@@ -1,32 +1,31 @@
 package com.demoqa.pages;
 
-import com.codeborne.selenide.SelenideElement;
+import com.demoqa.elements.*;
 import com.demoqa.pages.components.CalendarComponent;
 import io.qameta.allure.Step;
 
 import java.time.LocalDate;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.executeJavaScript;
 
 public class RegistrationFormPage {
     private static final String FORM_TITLE = "Student Registration Form";
     private CalendarComponent calendarComponent = new CalendarComponent();
-    private SelenideElement firstNameInput = $("#firstName"),
-            lastNameInput = $("#lastName"),
-            userEmailInput = $("#userEmail"),
-            genderRadioButton = $("#genterWrapper"),
-            userNumberInput = $("#userNumber"),
-            birthDateInput = $("#dateOfBirthInput"),
-            subjectsInput = $("#subjectsInput"),
-            hobbiesInput = $("#hobbiesWrapper"),
-            pictureUploader = $("#uploadPicture"),
-            currentAddressInput = $("#currentAddress"),
-            stateSelector = $("#stateCity-wrapper").$("#state"),
-            citySelector = $("#stateCity-wrapper").$("#city"),
-            submitButton = $("#submit");
+    private Input firstNameInput = new Input("firstName", $("#firstName"));
+    private Input lastNameInput = new Input("lastName", $("#lastName"));
+    private Input userEmailInput = new Input("userEmail", $("#userEmail"));
+    private Input userNumberInput = new Input("userNumber", $("#userNumber"));
+    private Input birthDateInput = new Input("dateOfBirth", $("#dateOfBirthInput"));
+    private Input subjectsInput = new Input("subjects", $("#subjectsInput"));
+    private Input currentAddressInput = new Input("currentAddress", $("#currentAddress"));
+    private RadioButton genderRadioButton = new RadioButton("gender", $("#genterWrapper"));
+    private Checkbox hobbiesCheckboxes = new Checkbox("hobbies", $("#hobbiesWrapper"));
+    private Button submitButton = new Button("submit", $("#submit"));
+    private PictureUploader pictureUploader = new PictureUploader("picture", $("#uploadPicture"));
+    private Select stateSelect = new Select("state", $("#stateCity-wrapper").$("#state"));
+    private Select citySelect = new Select("city", $("#stateCity-wrapper").$("#city"));
 
     @Step("Открываем страницу с формой automation-practice-form")
     public RegistrationFormPage openPage() {
@@ -39,31 +38,31 @@ public class RegistrationFormPage {
 
     @Step("Заполняем First Name значением '{firstName}'")
     public RegistrationFormPage setFirstName(String firstName) {
-        firstNameInput.setValue(firstName);
+        firstNameInput.setInputValue(firstName);
         return this;
     }
 
     @Step("Заполняем Last Name значением '{lastName}'")
     public RegistrationFormPage setLastName(String lastName) {
-        lastNameInput.setValue(lastName);
+        lastNameInput.setInputValue(lastName);
         return this;
     }
 
     @Step("Заполняем Email значением '{userEmail}'")
     public RegistrationFormPage setUserEmail(String userEmail) {
-        userEmailInput.setValue(userEmail);
+        userEmailInput.setInputValue(userEmail);
         return this;
     }
 
     @Step("Устанавливаем Gender в значение '{gender}'")
     public RegistrationFormPage setGender(String gender) {
-        genderRadioButton.$(byText(gender)).click();
+        genderRadioButton.setByLabelName(gender);
         return this;
     }
 
     @Step("Заполняем Mobile Number значением '{userNumber}'")
     public RegistrationFormPage setUserNumber(String userNumber) {
-        userNumberInput.setValue(userNumber);
+        userNumberInput.setInputValue(userNumber);
         return this;
     }
 
@@ -77,7 +76,7 @@ public class RegistrationFormPage {
     @Step("Заполняем Subjects значениями '{subjects}'")
     public RegistrationFormPage setSubjects(String[] subjects) {
         for (String subject : subjects) {
-            subjectsInput.setValue(subject).pressEnter();
+            subjectsInput.selectFromAutocompleteByFullValue(subject);
         }
         return this;
     }
@@ -85,38 +84,36 @@ public class RegistrationFormPage {
     @Step("Выбираем значения Hobbies '{hobbies}'")
     public RegistrationFormPage setHobbies(String[] hobbies) {
         for (String hobby : hobbies) {
-            hobbiesInput.$(byText(hobby)).click();
+            hobbiesCheckboxes.selectBoxByTitle(hobby);
         }
         return this;
     }
 
     @Step("Загружаем в Picture изображение '{picturePath}'")
     public RegistrationFormPage uploadPicture(String picturePath) {
-        pictureUploader.uploadFromClasspath(picturePath);
+        pictureUploader.uploadPicture(picturePath);
         return this;
     }
 
     @Step("Заполняем Current Address значением '{currentAddress}'")
     public RegistrationFormPage setCurrentAddress(String currentAddress) {
-        currentAddressInput.setValue(currentAddress);
+        currentAddressInput.setInputValue(currentAddress);
         return this;
     }
 
     @Step("Для State and City выбираем значение State '{state}'")
     public RegistrationFormPage setState(String state) {
-        stateSelector.click();
-        stateSelector.$(byText(state)).click();
+        stateSelect.selectValue(state);
         return this;
     }
 
     @Step("Для State and City выбираем значение City '{city}'")
     public RegistrationFormPage setCity(String city) {
-        citySelector.click();
-        citySelector.$(byText(city)).click();
+        citySelect.selectValue(city);
         return this;
     }
 
-    @Step("Нажимаем на кнопку Submit")
+    @Step("Загружаем форму")
     public void submitForm() {
         submitButton.click();
     }
--- src/test/java/com/demoqa/pages/components/CalendarComponent.java
+++ src/test/java/com/demoqa/pages/components/CalendarComponent.java
@@ -1,17 +1,27 @@
 package com.demoqa.pages.components;
 
+import com.demoqa.elements.Element;
+import com.demoqa.elements.NativeSelect;
+import io.qameta.allure.Step;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class CalendarComponent {
+    private NativeSelect yearSelect = new NativeSelect("year", $(".react-datepicker__year-select"));
+    private NativeSelect monthSelect = new NativeSelect("month", $(".react-datepicker__month-select"));
+    private Element dayElement;
+
+    @Step("Выбираем дату {date} в CalendarComponent")
     public CalendarComponent setDate(LocalDate date) {
-        $(".react-datepicker__year-select").selectOption(String.valueOf(date.getYear()));
-        $(".react-datepicker__month-select")
-                .selectOption(date.format(DateTimeFormatter.ofPattern("MMMM")));
-        $(".react-datepicker__day--" +
-                date.format(DateTimeFormatter.ofPattern("0dd")) + ":not(.react-datepicker__day--outside-month)").click();
+        yearSelect.selectValue(String.valueOf(date.getYear()));
+        monthSelect.selectValue(date.format(DateTimeFormatter.ofPattern("MMMM")));
+
+        dayElement =  new Element("day of month", $(".react-datepicker__day--" +
+                date.format(DateTimeFormatter.ofPattern("0dd")) + ":not(.react-datepicker__day--outside-month)"));
+        dayElement.click();
         return this;
     }
 }
